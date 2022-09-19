bee8602e3caad6770a98a50d5f163a6643373dd9
rjeancelestin521 <rjeancelestin521@gmail.com>
2022-09-19 19:10:41 +0300

    first commit

---
+++ src/main/java/mg/rjc/stock/StocksServiceApplication.java
@@ -0,0 +1,13 @@
+package mg.rjc.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksServiceApplication.class, args);
+    }
+
+}
